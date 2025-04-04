f1b9353f157c996cb15ed70525adf2859663f5ac
Aluno <germano.dc@aluno.ifsc.edu.br>
2025-04-04 14:33:53 -0300

    implementando o exer-3

--- exercicio-3/app/src/main/java/org/example/App.java
+++ exercicio-3/app/src/main/java/org/example/App.java
@@ -3,12 +3,45 @@
  */
 package org.example;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.util.Random;
 
+public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        char[][] tabuleiro = {{'.', '.', '.', '.', '.', '.', '.', '.', '.', '.',},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.',},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.',},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.',},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.',},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.',},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.',},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.',},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.',},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.',},}; //Matriz
+
+        Random numAleatorio1 = new Random();
+        Random numAleatorio2 = new Random();
+
+        int linha = numAleatorio1.nextInt(9);
+        int coluna = numAleatorio2.nextInt(9);
+
+        System.out.println(linha);
+        System.out.println(coluna);
+
+        //Porta avião
+        for (int i = 0; i < 5; i++){
+            if (coluna >= 6){
+                System.out.print("Não deu para colocar");
+                break;
+            }
+            tabuleiro[linha][coluna + i] = 'P';
+        }
+
+        for (int i = 0; i < 10; i++){
+            for (int j = 0; j < 10; j++){
+                System.out.print(tabuleiro[i][j]);
+            }
+            System.out.print("\n");
+        }
+
     }
 }
